1012e3f4dab7d004cf6df7c7777200e81d1dea35
Chris Koenig <chris.koenig@microsoft.com>
2011-10-12 21:18:29 -0500

    updating for posting

--- ReferenceDatabase/MainPage.xaml.cs
+++ ReferenceDatabase/MainPage.xaml.cs
@@ -3,14 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.IsolatedStorage;
 using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
 namespace ReferenceDatabase
